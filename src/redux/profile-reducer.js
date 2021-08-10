a31314625e21b7ff90851263df14afd1f7c15273
cybertrip-glitch <74563519+cybertrip-glitch@users.noreply.github.com>
2021-08-10 12:21:00 +0300

    refactor-profileReducer

--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -20,18 +20,27 @@ const profileReducer = (state = initialState, action) => {
                 message: state.newPostText,
                 likesCount: 0
             };
-            let stateCopy = {...state}
             
-            stateCopy.posts= [...state.posts];
-            stateCopy.posts.push(newPost)
-            stateCopy.newPostText = '';
-            return stateCopy;
+            
+            // stateCopy.posts= [...state.posts];
+            // stateCopy.posts.push(newPost)
+            // stateCopy.newPostText = '';
+            return  {
+                ...state,
+                posts:[...state.posts, newPost],
+                newPostText : ''
+         
+            }
+            
         }
         case UPDATE_NEW_POST_TEXT:{
-            let stateCopy = {...state}
+           
             
-            stateCopy.newPostText = action.newText;
-            return stateCopy;
+            //stateCopy.newPostText = action.newText;
+            return  {
+                ...state,
+                newPostText: action.newText
+            }
         }
         default:
             return state;
